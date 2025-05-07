b6056b0a2dd63172594ae7a165c45554e1e16ecb
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-05-06 22:52:23 -0500

    feat: enhance getCountries method to sort countries and their teams

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { CountryModel } from '../models/country.model';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,13 @@ export class CountriesService {
   private readonly httpClient = inject(HttpClient);
 
   getCountries() {
-    return this.httpClient.get<CountryModel[]>('/api/countries');
+    return this.httpClient.get<CountryModel[]>('/api/countries').pipe(
+      map((countries) => countries
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((country) => ({
+        ...country,
+        teams: country.teams.sort((a, b) => a.name.localeCompare(b.name))
+      })))
+    );
   }
 }
