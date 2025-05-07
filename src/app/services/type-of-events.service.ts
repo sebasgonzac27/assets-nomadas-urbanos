a4403b340d261c2447f883e41cfab40372a09df7
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-05-06 22:31:30 -0500

    style: format imports in type-of-events.service.ts for consistency

--- src/app/services/type-of-events.service.ts
+++ src/app/services/type-of-events.service.ts
@@ -1,6 +1,6 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {TypeOfEventModel} from '../models/type-of-event.model';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TypeOfEventModel } from '../models/type-of-event.model';
 
 @Injectable({
   providedIn: 'root'
